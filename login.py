0debd7743f3cd24d1c4a221ad1c66c366f98b025
Matheus Fellipe <bentomatheus48@gmail.com>
2022-11-18 10:50:07 -0300

    commit

--- login.py
+++ login.py
@@ -33,6 +33,7 @@ l_nome.place(x=10, y=48)
 
 acesso = ['matheus', '123456']
 
+#Função verificação de acesso
 def checar_senha():
     nome = e_nome.get()
     senha = e_senha.get()
@@ -41,10 +42,30 @@ def checar_senha():
         messagebox.showinfo('Login', 'Acesso permitido')
     elif acesso[0] == nome and acesso[1]==senha:
         messagebox.showinfo('Acesso permitido', 'seja bem vindo novamente '  +acesso[0])
+
+        #Isto irá deletar tudo nos frames em cima e em baixo
+        for widget in frame_baixo.winfo_children():
+            widget.destroy()
+
+        for widget in frame_cima.winfo_children():
+            widget.destroy()
+
+        nova_janela()
+
     else:
         messagebox.showwarning('Acesso negado', 'Verifique as credenciais')
         
 
+#Função para abrir nova janela após acesso
+def nova_janela():
+    l_nome = Label(frame_cima, text='Usuario : ' +acesso[0], anchor=NE, font=('Ivy 20'), bg=co1, fg=co4)
+    l_nome.place(x=5, y=5)
+
+    l_nome = Label(frame_cima, text='', width=275, anchor=NE, font=('Ivy 1'), bg=co2, fg=co4)
+    l_nome.place(x=10, y=48)
+
+    l_nome = Label(frame_baixo, text='Bem vindo ' +acesso[0], anchor=NE, font=('Ivy 20'), bg=co1, fg=co4)
+    l_nome.place(x=5, y=105)
 
 #Configurando Parte baixo
 l_nome = Label(frame_baixo, text='Nome *', anchor=NW, font=('Ivy 11'), bg=co1, fg=co4)
@@ -60,10 +81,6 @@ e_senha.place(x=14, y=130)
 
 b_confirmar = Button(frame_baixo, command=checar_senha, text='Entrar', width=39, height=2, font=('Ivy 8 bold'), bg=co2, fg=co1, relief=RAISED, overrelief=RIDGE)
 b_confirmar.place(x=15, y=180)
-
-
-
-
 
 
 
