2657a9281ee203bf3da437eaea20d79852ceec56
Matheus Fellipe <bentomatheus48@gmail.com>
2022-11-18 10:35:39 -0300

    ajustes

--- login.py
+++ login.py
@@ -1,13 +1,13 @@
 from tkinter import *
 from tkinter import Tk, ttk
-
+from tkinter import messagebox
 
 #cores
 co0 = "#f0f3f5"  # Preta 
-co1 = "#feffff"  # branca
-co2 = "#3fb5a3"  # verde 
+co1 = "#282a36"  # branca
+co2 = "#50fa7b"  # verde 
 co3 = "#38576b"  # valor 
-co4 = "#403d3d"   # letra 
+co4 = "#f8f8f2"   # letra 
 
 
 janela = Tk()
@@ -28,12 +28,38 @@ l_nome = Label(frame_cima, text='Login', anchor=NE, font=('Ivy 25'), bg=co1, fg=
 l_nome.place(x=5, y=5)
 
 l_nome = Label(frame_cima, text='', width=275, anchor=NE, font=('Ivy 1'), bg=co2, fg=co4)
-l_nome.place(x=10, y=46)
+l_nome.place(x=10, y=48)
+
+
+acesso = ['matheus', '123456']
+
+def checar_senha():
+    nome = e_nome.get()
+    senha = e_senha.get()
+
+    if nome =='admin' and senha == 'admin':
+        messagebox.showinfo('Login', 'Acesso permitido')
+    elif acesso[0] == nome and acesso[1]==senha:
+        messagebox.showinfo('Acesso permitido', 'seja bem vindo novamente '  +acesso[0])
+    else:
+        messagebox.showwarning('Acesso negado', 'Verifique as credenciais')
+        
+
 
 #Configurando Parte baixo
-l_nome = Label(frame_baixo, text='Nome *', anchor=NW, font=('Ivy 10'), bg=co1, fg=co4)
+l_nome = Label(frame_baixo, text='Nome *', anchor=NW, font=('Ivy 11'), bg=co1, fg=co4)
 l_nome.place(x=10, y=20)
 e_nome = Entry(frame_baixo, width=25, justify='left', font=('', 15), highlightthickness=1, relief='solid' )
+e_nome.place(x=14, y=50)
+
+l_senha = Label(frame_baixo, text='Senha *', anchor=NW, font=('Ivy 11'), bg=co1, fg=co4)
+l_senha.place(x=10, y=95)
+e_senha = Entry(frame_baixo, width=25, justify='left', show='*' ,font=('', 15), highlightthickness=1, relief='solid' )
+e_senha.place(x=14, y=130)
+
+
+b_confirmar = Button(frame_baixo, command=checar_senha, text='Entrar', width=39, height=2, font=('Ivy 8 bold'), bg=co2, fg=co1, relief=RAISED, overrelief=RIDGE)
+b_confirmar.place(x=15, y=180)
 
 
 
